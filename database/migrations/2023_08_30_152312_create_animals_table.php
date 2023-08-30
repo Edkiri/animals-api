552d95cee3a157f887f90f4314f91664f61216d6
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-08-30 17:43:11 +0200

    feat: migration created for animals db table

---
+++ database/migrations/2023_08_30_152312_create_animals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('species');
+            $table->unsignedSmallInteger('age');
+            $table->string('gender')->checkIn(['male', 'female']);
+            $table->unsignedSmallInteger('weight');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
